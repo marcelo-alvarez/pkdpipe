853955c71f7e8d5f1e1879ec5ec0bd2b651b2dac
Marcelo Alvarez <{ID}+{username}@users.noreply.github.com>
2025-02-18 17:29:37 -0800

    streamline io

--- scripts/lightcone_plotter.py
+++ scripts/lightcone_plotter.py
@@ -7,9 +7,11 @@ from lightcone_reader import lightcone_reader
 import ast
 import sys
 
-dbounds = [[-1,1],[-1,1],[-0.002,0.002]]
+dbounds = [[0,1],[0,1],[-0.0005,0.0005]]
 dnproc=12
 dcname = "test"
+dpsize = 0.02
+ddpi   = 900
 
 # parse command line
 parser = argparse.ArgumentParser(description='Collects the lightcone output of pkdgrav to shells.')
@@ -17,40 +19,44 @@ parser.add_argument('--param', type=str,              help='Path to the param fi
 parser.add_argument('--bounds',type=ast.literal_eval, help=f'bounds [[xmn,xmx],[ymn,ymx],[zmn,zmx]]; default: {dbounds}', default=dbounds)
 parser.add_argument('--nproc', type=int,              help=f'nproc: {dnproc}', default=dnproc)
 parser.add_argument('--cname', type=str,              help=f'cname: particles cached in cname.npy output in cname.png', default=dcname)
+parser.add_argument('--psize', type=float,            help=f'particle size in plot', default=dpsize)
+parser.add_argument('--dpi',   type=int,              help=f'dpi of output image', default=ddpi)
 
 args = parser.parse_args()
 
 # get particles
-pfile = f"{args.cname}.npy"
-print(pfile)
+pfile = f"{args.cname}.npz"
 if os.path.exists(pfile):
     print(f"using cached particles in {pfile}")
-    parts = np.load(pfile)
+    data = np.load(pfile)
+    parts = data['parts']
+    boxsize = data['boxsize'][0]
 else:
-    parts = lightcone_reader(args.param, args.bounds, args.nproc)
-    np.save(pfile,parts)
+    boxsize, parts = lightcone_reader(args.param, args.bounds, args.nproc)
+    np.savez(pfile,parts=parts,boxsize=np.asarray([boxsize]))
 
-x = parts['x']
-y = parts['y']
-z = parts['z']
+boxsize /= 1e3 # Mpc/h --> Gpc/h
+
+x = boxsize * parts['x']
+y = boxsize * parts['y']
+z = boxsize * parts['z']
 vx = parts['vx']
 vy = parts['vy']
 
-vx -= vx.mean() ; vy -= vy.mean()
+npart = len(x)
+
 vr = (x*vx + y*vy)/np.sqrt(x**2+y**2)
 vr /= vr.max()
-vr[vr<-vr.max()] = vr.max()
 
-basic_cols=['red', 'grey', 'blue']
-cmap = LinearSegmentedColormap.from_list(
-    "cmap_name",
-    basic_cols
-)
 cmap='seismic'
 plt.gca().set_facecolor('black')
-plt.scatter(x,y,facecolors=vr,s=0.05,linewidth=0,c=vr,edgecolors=None,cmap=cmap)
+plt.scatter(x,y,facecolors=vr,s=args.psize,linewidth=0,c=vr,edgecolors=None,cmap=cmap)
 plt.gca().set_aspect('equal')
 
-plt.gca().set_xlim((0,1))
-plt.gca().set_ylim((0,1))
-plt.savefig(args.cname,dpi=900)
+plt.gca().set_xlim((0,boxsize))
+plt.gca().set_ylim((0,boxsize))
+
+plt.gca().set_xlabel('x [Gpc/h]')
+plt.gca().set_ylabel('y [Gpc/h]')
+
+plt.savefig(args.cname,dpi=args.dpi)
--- scripts/lightcone_reader.py
+++ scripts/lightcone_reader.py
@@ -10,8 +10,8 @@ import numpy.lib.recfunctions as recfc
 Collects the lightcone output and renames the files
 """
 
-def process_single(n_step,namespace,z_output,lcp_type,z_out,bounds):
-    cparts = []
+def process_single(n_step,namespace,z_output,z_out,bounds):
+    parts = np.zeros((6,0),dtype=np.float32)
     current_proc = 0
     while True:
         # if we expect everything to be empty just remove the files
@@ -26,29 +26,33 @@ def process_single(n_step,namespace,z_output,lcp_type,z_out,bounds):
                 """
                 The output in the lcp file contains particle positions and velocities
                 """
-                parts = np.fromfile(current_file, count=-1, dtype=lcp_type)
-                parts = recfc.drop_fields(parts, ["mass","eps","phi"], usemask=False)                    
-                # add group parts to the other (from tools in pkdgrav3)
-                #assert np.sum(data[0::3]) == 0, "There are apparently grouped parts..."
-                cparts.append(parts)
+                cparts = np.fromfile(current_file, dtype=np.float32).reshape((-1,10))
+                dm = ((cparts[:,2]>bounds[0][0]) & (cparts[:,2]<bounds[0][1]) &
+                      (cparts[:,3]>bounds[1][0]) & (cparts[:,3]<bounds[1][1]) &
+                      (cparts[:,4]>bounds[2][0]) & (cparts[:,4]<bounds[2][1]))
+                cparts = cparts[dm,2:8].transpose()
+                if np.shape(cparts)[1] > 0:
+                    parts = np.concatenate((parts,cparts),axis=1)
                 current_proc += 1
-            else:
-                # concat and truncate
-                cparts = np.concatenate(cparts)
+            elif current_proc > 1:
+                # update shell boundaries
+                z_0 = z_output[n_step]
 
+                # catch very small z
+                if np.abs(z_0) <= 1e-9:
+                    z_0 = 0.0
+
+                z_1 = z_output[n_step - 1]
+
+                # catch larger than output redshifts
+                if z_1 > z_out:
+                    z_1 = z_out
+
+                # break the loop
                 print(f"completed step {n_step:>4}",end='\r')
                 break
 
-    if len(cparts) == 0:
-        return None
-    lcp_type = np.dtype([("x",'f'),("y",'f'),("z",'f'),("vx",'f'),("vy",'f'),("vz",'f')])
-    cparts = cparts.astype(lcp_type)
-    dm = ((cparts['x']>bounds[0][0]) & (cparts['x']<bounds[0][1]) &
-          (cparts['y']>bounds[1][0]) & (cparts['y']<bounds[1][1]) &
-          (cparts['z']>bounds[2][0]) & (cparts['z']<bounds[2][1]))
-    cparts = np.vstack((cparts['x'][dm],   cparts['y'][dm],  cparts['z'][dm],
-            cparts['vx'][dm], cparts['vy'][dm], cparts['vz'][dm]))
-    return cparts
+    return parts
 
 
 def lightcone_reader(param_file, bounds, nproc):
@@ -61,28 +65,29 @@ def lightcone_reader(param_file, bounds, nproc):
     namespace = re.search(r'(?<=achOutName)\s*=\s*["\'].+["\']', content)[0].split("=")[-1].strip().replace('"', '').replace("'", "")
     print(f"achOutName:   ", namespace, flush=True)
 
-    nside = int(re.search(r"(?<=nSideHealpix)\s*=\s*\d+", content)[0].split("=")[-1].strip())    
-    print(f"nSideHealpix: ", nside, flush=True)
+    lcfile = f"{namespace}.lcp"
+    if os.path.exists(lcfile):
+        return lcfile
+
     z_out = float(re.search(r"(?<=dRedshiftLCP)\s*=\s*\d+\.\d+", content)[0].split("=")[-1].strip())
     print(f"dRedshiftLCP: ", z_out, flush=True)
     steps = int(re.search(r"(?<=nSteps)\s*=\s*\d+", content)[0].split("=")[-1].strip())
     print(f"nSteps:       ", steps, flush=True)
+    boxsize = int(re.search(r"(?<=dBoxSize)\s*=\s*\d+", content)[0].split("=")[-1].strip())
+    print(f"BoxSize:      ", boxsize, flush=True)
     print()
     
     # read out the log file
     z_output = np.genfromtxt(namespace + ".log")[:, 1]
-    lcp_type = np.dtype([("mass",'d'),( "x",'f'),( "y",'f'),( "z",'f'),
-                                     ("vx",'f'),("vy",'f'),("vz",'f'),
-                                     ("eps",'f'),("phi",'f')])
 
-    args = [[n_step,namespace,z_output,lcp_type,z_out,bounds] for n_step in range(1, steps + 1)]
+    args = [[n_step,namespace,z_output,z_out,bounds] for n_step in range(1, steps + 1)]
     with mp.Pool(processes=nproc) as pool:
         multi_collect = pool.starmap(process_single,args)
 
     allpart = np.zeros((6,0))
-    for p in multi_collect:
-        part = p#.get()
-        if part is not None:
+    for part in multi_collect:
+        if np.shape(part)[1]>0: # is not None:
             allpart = np.concatenate((allpart,part),axis=1)
     print()
-    return np.rec.fromarrays(allpart,names=['x','y','z','vx','vy','vz'])
+
+    return boxsize,np.rec.fromarrays(allpart.reshape((6,-1)),names=['x','y','z','vx','vy','vz'])
