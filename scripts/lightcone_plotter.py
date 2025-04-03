3ac75fa0ca302c55d92625bb5012790190ba4137
Marcelo Alvarez <{ID}+{username}@users.noreply.github.com>
2025-04-03 13:55:05 -0700

    refactor

--- scripts/lightcone_plotter.py
+++ scripts/lightcone_plotter.py
@@ -5,44 +5,10 @@ import os.path
 import ast
 from typing import Dict, Any
 from pkdpipe.datainterface import DataInterface
+from cli import parse_command_line, dict_hash
 
-def dict_hash(dictionary: Dict[str, Any]) -> str:
-    from typing import Dict, Any
-    import hashlib
-    import json
+args = parse_command_line()
 
-    """
-    MD5 hash of a dictionary
-    """
-    dhash = hashlib.md5()
-    encoded = json.dumps(dictionary, sort_keys=True).encode()
-    dhash.update(encoded)
-    return str(dhash.hexdigest())[:10]
-
-dparam = "/global/cfs/cdirs/cosmosim/slac/malvarez/pkdgrav3/scaling-tests/runs/N1400-L1050-008gpus/N1400-L1050-008gpus.par"
-dbbox  = [[-1,1],[-1,1],[-0.0005,0.0005]]
-dnproc=12
-dcname = None
-dpsize = 0.02
-ddpi   = 900
-dcmap  = "seismic"
-ddtype = "particles"
-
-# parse command line
-parser = argparse.ArgumentParser(description='Collects the lightcone output of pkdgrav to shells.')
-parser.add_argument('--param', type=str,              help=f'Path to the param file used to start the sim', default=dparam)
-parser.add_argument('--bbox',  type=ast.literal_eval, help=f'bbox [[xmn,xmx],[ymn,ymx],[zmn,zmx]]; default: {dbbox}', default=dbbox)
-parser.add_argument('--nproc', type=int,              help=f'nproc: {dnproc}', default=dnproc)
-parser.add_argument('--cname', type=str,              help=f'cname: output in cname.png', default=dcname)
-parser.add_argument('--psize', type=float,            help=f'particle size in plot', default=dpsize)
-parser.add_argument('--dpi',   type=int,              help=f'dpi of output image', default=ddpi)
-parser.add_argument('--cmap',  type=str,              help=f'colormap of output image', default=dcmap)
-parser.add_argument('--dtype', type=str,              help=f'data type', default=ddtype)
-
-args = parser.parse_args()
-if args.cname is None:
-    args.cname = args.dtype
-    
 pkdata = DataInterface(param_file=args.param,nproc=args.nproc)
 
 boxsize = pkdata.params['boxsize']
@@ -56,8 +22,8 @@ elif args.dtype == "halos":
     dataset='xvh'
     filetype='fof'
 
-# get particles
-pfile = f"__pkdpipe-cache__/{runcode}.npz"
+# get data
+pfile = f"__pkdpipe-cache__/plotter-{runcode}.npz"
 os.makedirs(os.path.dirname(pfile), exist_ok=True)
 if os.path.exists(pfile):
     print(f"using cached data in {pfile}")
