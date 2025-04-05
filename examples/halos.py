05a1298154a71ea905ff991afa100fc87fc63517
Marcelo Alvarez <{ID}+{username}@users.noreply.github.com>
2025-04-04 18:19:16 -0700

    update halos.py with mass and scratch path

--- examples/halos.py
+++ examples/halos.py
@@ -7,21 +7,21 @@ nproc=64
 bbox  = [[-1,1],[-1,1],[-0.0005,0.0005]]
 
 # this is the pkdgrav3 parameter file for the run with halos we want to access
-simpath = "/global/cfs/cdirs/cosmosim/slac/malvarez/pkdgrav3/scaling-tests/runs/"
+simpath = "/pscratch/sd/m/malvarez/pkdgrav3/scaling-tests/"
 
-param_file = f"{simpath}/N1400-L1050-008gpus/N1400-L1050-008gpus_v0.par"
+param_file = f"{simpath}/N1400-L1050-008gpus/N1400-L1050-008gpus.par"
 #param_file = f"{simpath}/N2800-L2100-064gpus/N2800-L2100-064gpus.par"
 
 pkdata = DataInterface(param_file=param_file,nproc=nproc)
 parts = pkdata.fetch_data(bbox,dataset='xvh',filetype='fof')
 
-boxsize = pkdata.params['boxsize']
-
-boxsize /= 1e3 # Mpc/h --> Gpc/h
+boxsize  = pkdata.params['boxsize'] / 1e3 # Gpc/h
+pmass    = 2.775e20 * pkdata.params['omegam'] * boxsize**3 / pkdata.params['ngrid']**3 # Msun/h
 
 x = boxsize * parts['x']
 y = boxsize * parts['y']
-z = boxsize * parts['z']
+
 vx = parts['vx']
 vy = parts['vy']
 
+Mhalo = data[box]['npart'] * pmass # Msun/h
