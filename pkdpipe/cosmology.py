870cf6cbbbbef846f2db93bad6f7de121006eaf1
Marcelo Alvarez <{ID}+{username}@users.noreply.github.com>
2025-04-10 00:14:07 -0700

    add simulation

--- pkdpipe/cosmology.py
+++ pkdpipe/cosmology.py
@@ -7,71 +7,135 @@ import camb
 pkdpipe cosmology
 """
 
-pkdp_cosmo_defaults = {
-    'h':      0.67,
-    'omegam': 0.32,
-    'omegab': 0.049,
-    'omegak': 0.0,
-    'sigma8': 0.831,
-    'As':     2.1e-9,
-    'ns':     0.96,
-    'mnu':    0.0587,
-    'tau':    0.0543,
-    'numnu':  3,
-    'harchy': 'normal',
-    'k0phys': 0.05, # pivot scale in 1/Mpc
-}
+def get_cosmology(cosmoname):
+    # default is DESI DR2+Planck+ACT parameters with no nu prior but ignoring mnu by setting mnu=0
+    h     = 68.906658 / 100
+    ombh2 = 0.022597077
+    omch2 = 0.11788858
+    As    = 2.1302742e-09
+    ns    = 0.97278164
+    cosmoparams = {
+            'h':      h,
+            'omegam': (ombh2+omch2) / h**2,
+            'omegab': ombh2 / h**2,
+            'As':     As,
+            'ns':     ns,
+            'TCMB':   2.72548,
+            'omegak': 0.0,
+            'sigma8': None,
+            'mnu':    0,
+            'nnu':    3.044,
+            'tau':    0.0543,
+            'numnu':  3,
+            'harchy': 'normal',
+            'k0phys': 0.05,
+        }
+    if cosmoname == 'desi-dr2-planck-act-mnufree':
+        pass
+    elif cosmoname == 'euclid-flagship':
+        cosmoparams['h']      = 0.67
+        cosmoparams['omegam'] = 0.32
+        cosmoparams['omegab'] = 0.049
+        cosmoparams['sigma8'] = 0.831
+        cosmoparams['As']     = 2.1e-9
+        cosmoparams['ns']     = 0.96
+        cosmoparams['mnu']    = 0.0587
+    return cosmoparams
 
 class Cosmology:
 
     '''Cosmology'''
     def __init__(self, **kwargs):
-        self.params = {}
-        for key in pkdp_cosmo_defaults:
-            self.params[key] = kwargs.get(key,pkdp_cosmo_defaults[key])
 
-        h      = self.params['h']
-        omegam = self.params['omegam']
-        omegab = self.params['omegab']
-        omegak = self.params['omegak']
-        sigma8 = self.params['sigma8']
-        As     = self.params['As']
-        ns     = self.params['ns']
-        mnu    = self.params['mnu']
-        tau    = self.params['tau']
-        numnu  = self.params['numnu']
-        harchy = self.params['harchy']
-        k0phys = self.params['k0phys']
+        self.cosmoname = kwargs.get('cosmology','default')
+        self.params = get_cosmology(self.cosmoname)
+        for key in self.params:
+            self.params[key] = kwargs.get(key,self.params[key])
    
-        H0    = 100 * h
-        ombh2 = omegab * h**2
-        omch2 = (omegam - omegab) * h**2
-        omk   = omegak
-        k0    = k0phys / h # pivot scale in h/Mpc
+        k0   = self.params['k0phys'] / self.params['h'] # pivot scale in h/Mpc
+        kmin = 5e-5
+        kmax = 2e1
 
         self.cambpars = camb.set_params(
-            H0=H0,
-            As=As,
-            ombh2=ombh2,
-            omch2=omch2,
-            mnu=mnu,
-            omk=omk,
-            tau=tau,
-            ns=ns,
-            num_massive_neutrinos=numnu,
-            neutrino_hierarchy=harchy)
+            TCMB                  = self.params['TCMB'],
+            H0                    = 100 * self.params['h'],
+            As                    = self.params['As'],
+            ombh2                 = self.params['omegab'] * self.params['h']**2,
+            omch2                 = (self.params['omegam'] - self.params['omegab']) * self.params['h']**2,
+            mnu                   = self.params['mnu'],
+            nnu                   = self.params['nnu'],
+            omk                   = self.params['omegak'],
+            tau                   = self.params['tau'],
+            ns                    = self.params['ns'],
+            num_massive_neutrinos = self.params['numnu'],
+            neutrino_hierarchy    = self.params['harchy'])
 
         self.cambpars.NonLinear = camb.model.NonLinear_none
-        self.cambpars.set_matter_power(redshifts=[0,], kmax=10.0)
-
+        self.cambpars.set_matter_power(redshifts=[0,], kmax=kmax, accurate_massive_neutrino_transfers=True)
         self.camb_results = camb.get_results(self.cambpars)
-        self.kpk, self.zpk, self.pk = self.camb_results.get_matter_power_spectrum(
-            minkh=1e-4, maxkh=10, npoints = 1000)
-        self.sigma8_norm = (sigma8 / self.camb_results.get_sigma8_0())**2
+        self.k, self.z, self.pk = self.camb_results.get_matter_power_spectrum(
+            minkh=kmin, maxkh=kmax, npoints = 400)
 
-        # normalize using sigma8
-        self.pk = self.pk[0] * self.sigma8_norm
+        self.pk = self.pk[0]
+
+        if self.params['sigma8'] is not None:
+            # normalize using sigma8
+            self.sigma8_norm = (self.params['sigma8'] / self.camb_results.get_sigma8_0())**2
+            self.pk = self.pk * self.sigma8_norm
+        else:
+            self.params['sigma8'] = self.camb_results.get_sigma8_0()
 
         # get z=0 transfer function
-        self.T = np.sqrt(self.pk/As/(self.kpk/k0)**ns)
+        self.T = np.sqrt(self.pk/self.params['As']/(self.k/k0)**self.params['ns'])
 
+        # generate summary
+        self._generate_cosmosummary()
+
+        # add omegal and sigma8 to cosmo parameters
+        self.params['omegal'] = self.camb_results.get_Omega(var='de')
+
+        # redshift and comoving distance grids
+        self._zgrid = np.logspace(-5, 6, num=10000)
+        self._chigrid = self.camb_results.comoving_radial_distance(self._zgrid)
+
+    def z2chi(self, z):
+        return np.interp(z, self._zgrid, self._chigrid)
+
+    def chi2z(self, chi):
+        return np.interp(chi, self._chigrid, self._zgrid)
+
+    def _generate_cosmosummary(self):
+        self.cosmosummary = "\nValues used in CAMB for this transfer function\n\n"
+        width=17
+        omegas = {}
+        h  = self.cambpars.H0 / 100
+        sigma8 = self.camb_results.get_sigma8_0()
+        Neff = sum(self.cambpars.nu_mass_degeneracies[:self.cambpars.nu_mass_eigenstates]) + self.cambpars.num_nu_massless
+        omega = 0.0
+        for var in ['cdm', 'baryon', 'photon', 'neutrino', 'nu', 'de']:
+            omegas[var] = self.camb_results.get_Omega(var=var)
+            omega += omegas[var]
+        self.cosmosummary += f'{"As:":>{width}} {self.cambpars.InitPower.As:1.4e}\n'
+        self.cosmosummary += f'{"omega_b h^2:":>{width}} {omegas['baryon']*h**2:1.4e}\n'
+        self.cosmosummary += f'{"omega_c h^2:":>{width}} {omegas['cdm']*h**2:1.4e}\n'
+        self.cosmosummary += f'{"H0:":>{width}} {self.cambpars.H0:1.4e}\n'
+        self.cosmosummary += f'{"ns:":>{width}} {self.cambpars.InitPower.ns:1.4e}\n'
+        self.cosmosummary += '\n'
+        self.cosmosummary += f'{"sigma8:":>{width}} {sigma8:1.4e}\n'
+        self.cosmosummary += f'{"omega_nu:":>{width}} {omegas['nu']:1.4e}\n'
+        self.cosmosummary += '\n'
+        self.cosmosummary += f'{"Neff:":>{width}} {Neff:1.4e}\n'
+        self.cosmosummary += f'{"omega_r,gamma:":>{width}} {omegas['photon']:1.4e}\n'
+        self.cosmosummary += f'{"omega_r,nu:":>{width}} {omegas['neutrino']:1.4e}\n'
+        self.cosmosummary += '\n'
+        self.cosmosummary += f'{"omega_l:":>{width}} {omegas['de']:1.4e}\n'
+        self.cosmosummary += f'{"omega_r:":>{width}} {(omegas['neutrino']+omegas['photon']):1.4e}\n'
+        self.cosmosummary += f'{"omega_m:":>{width}} {omegas['baryon']+omegas['cdm']+omegas['nu']:1.4e}\n'
+        self.cosmosummary += f'{"omega:":>{width}} {omegas['de']+omegas['neutrino']+omegas['photon']+omegas['baryon']+omegas['cdm']+omegas['nu']:1.4e}\n'
+
+    def writetransfer(self,filename):
+        header = self.cosmosummary
+        header += f'\n'
+        header += f' Column 1: k [h/Mpc]\n'
+        header += f' Column 2: T(k) = sqrt(P(k,z=0) / As / (k/(0.05 1/Mpc))^ns) [Mpc^(3/2)]\n'
+        np.savetxt(filename, np.transpose([self.k, self.T]),fmt='%1.5e', header=header)
---
+++ pkdpipe/simulation.py
@@ -0,0 +1,191 @@
+import numpy as np
+import sys
+import pathlib
+import subprocess
+import os
+import shutil
+from string import Template
+from pkdpipe.cosmology import Cosmology
+from pkdpipe.cli import parsecommandline
+
+"""
+pkdpipe simulation
+"""
+
+djobname = "N{ngrid}-L{lbox}-{nodes*4}gpus"
+demail   = "${pkdgravemail}"
+dsimname = "lcone-small"
+
+templatedir = f"{str(pathlib.Path(__file__).parent.resolve())}/../templates"
+scriptdir   = f"{str(pathlib.Path(__file__).parent.resolve())}/../scripts"
+
+partemp   = f"{templatedir}/lightcone.par"
+slurmtemp = f"{templatedir}/pkdgrav.sh"
+runtemp   = f"{scriptdir}/run.sh"
+
+def copytemplate(templatefile,outfile,data):
+    with open(templatefile, "r") as file:
+        templines = file.read()
+    template = Template(templines)
+    lines = template.substitute(data)
+    with open(outfile, 'w') as par:
+        par.write(lines)
+
+def get_simulation(simname):
+    dpath    = f"{os.getenv('SCRATCH')}/pkdgrav3/runs"
+    dcosmo = 'desi-dr2-planck-act-mnufree'
+    redshiftlist = sorted([3. , 2.81388253, 2.6350418 , 2.46500347, 2.30360093,
+       2.1496063 , 2.003003  , 1.86204923, 1.72851296, 1.60145682,
+       1.48015873, 1.36406619, 1.25377507, 1.1486893 , 1.04834084,
+       0.9527436 , 0.8615041 , 0.77462289, 0.69176112, 0.61290323,
+       0.53751538, 0.46584579, 0.39742873, 0.33226752, 0.27000254,
+       0.21065375, 0.15420129, 0.10035211, 0.04898773, 0.        ] + 
+       [0.295, 0.510, 0.706, 0.934, 1.321, 1.484, 2.33, 0.922, 0.955],reverse=True)
+    ddRedTo = f'{[f'{z:0.4f}' for z in redshiftlist]}'.replace("'", "")
+    dnSteps = f'{[f'{i}' for i in range(len(redshiftlist))]}'.replace("'", "")
+
+    params = {
+        'ngrid'  : {'val' :       1400, 'type' :   int, 'desc' : 'cube root particle number'},
+        'lbox'   : {'val' :       1050, 'type' : float, 'desc' : 'boxsize [Mpc/h]'},
+        'nodes'  : {'val' :          2, 'type' :   int, 'desc' : 'number of nodes'},
+        'gpuper' : {'val' :          4, 'type' :   int, 'desc' : 'GPUs per node'},
+        'rundir' : {'val' :      dpath, 'type' :   str, 'desc' : 'path for runs'},
+        'jobname': {'val' :   djobname, 'type' :   str, 'desc' : 'directory name for run'},
+        'simname': {'val' :   dsimname, 'type' :   str, 'desc' : 'simulation parameter set name'},
+        'email'  : {'val' :     demail, 'type' :   str, 'desc' : 'email for slurm notifications'},
+        'tlimit' : {'val' : '48:00:00', 'type' :   str, 'desc' : 'time limit'},
+        'cosmo'  : {'val' :     dcosmo, 'type' :   str, 'desc' : 'cosmology'},
+        'dRedTo' : {'val' :    ddRedTo, 'type' :   str, 'desc' : 'output redshifts'},
+        'nSteps' : {'val' :    dnSteps, 'type' :   str, 'desc' : 'output steps'},
+        'sbatch' : {'val' :      False, 'type' :  bool, 'desc' : 'submit with sbatch; otherwise only create dir & files'}}
+
+    # only lightcone tests are implemented so far
+    params['jobname']['val'] = simname
+    if simname == 'lcone-medium':
+        pass
+    elif simname == 'lcone-small':
+        params['ngrid']['val'] = 700
+        params['nodes']['val'] = 1
+    elif simname == 'lcone-large':
+        params['ngrid']['val'] = 2800
+        params['nodes']['val'] = 16
+    return params
+
+class Simulation:
+
+    '''Simulation'''
+    def __init__(self, **kwargs):
+
+        self.simname = kwargs.get('simname', dsimname)
+
+        # initialize parameters using parser
+        self.params = parsecommandline(get_simulation(self.simname))
+
+        if kwargs.get('parse', False):
+            # if requested parse again to retrieve updated simulation name from command-line
+            self.simname = self.params['simname']
+            self.params = parsecommandline(get_simulation(self.simname))
+
+        # update parameters via kwargs
+        for key in self.params:
+            self.params[key] = kwargs.get(key,self.params[key])
+
+    def update_params(self, params):
+        self.params = get_simulation(params['simname'])
+        print(self.params['simname'])
+        for key in params:
+            self.params[key] = params[key]
+
+    def submit(self):
+
+        ngrid   = self.params['ngrid']
+        lbox    = self.params['lbox']
+        nodes   = self.params['nodes']
+        rundir  = self.params['rundir']
+        jobname = self.params['jobname']
+        tlimit  = self.params['tlimit']
+        email   = self.params['email']
+        sbatch  = self.params['sbatch']
+
+        gpus = nodes * self.params['gpuper']
+
+        if jobname == djobname:
+            jobname = f"N{ngrid:04d}-L{lbox}-{gpus:03d}gpus"
+            
+        if email == demail:
+            email = os.getenv('pkdgravemail')
+
+        jobdir  = f"{rundir}/{jobname}"
+        outdir  = f"{jobdir}/output"
+
+        transfer  = f"{jobdir}/{jobname}-transfer.dat"
+
+        if os.path.isdir(jobdir):
+            response = input(f"{jobdir} already exists; enter REMOVE if you want to delete it\n")
+            if response == "REMOVE":
+                nsleep=10
+                for i in range(nsleep):
+                    print(f"will delete {jobdir} in {nsleep-i} seconds...",end='\r')
+                    subprocess.call(f"sleep 1", shell=True)
+                print()
+                if len(jobdir) > 10:
+                    subprocess.call(f"rm -rf {jobdir}", shell=True)
+                else:
+                    print(f"exiting...")
+                    sys.exit(1)
+            else:
+                print(f"exiting...")
+                sys.exit(1)
+
+        os.makedirs(outdir)
+        print(f"created {outdir}")
+
+        runscript = f"{jobdir}/run.sh"
+        shutil.copy(runtemp, runscript)
+        print(f"created {runscript}")
+
+        # get cosmology
+        cosmo  = Cosmology(cosmology='desi-dr2-planck-act-mnufree')
+
+        # get maximum redshift
+        zmax = f"{cosmo.chi2z(lbox/cosmo.params['h'])+0.01:0.2f}"
+
+        # write transfer file
+        cosmo.writetransfer(transfer)
+        print(f"created {transfer}")
+
+        # parameter file
+        parfile = f"{jobdir}/{jobname}.par"
+        copytemplate(partemp,parfile,{
+                    "jobname" : jobname,
+                    "transfer": transfer,
+                    "jobdir"  : jobdir,
+                    "outdir"  : outdir,
+                    "zmax"    : zmax,
+                    "ngrid"   : ngrid,
+                    "lbox"    : lbox,
+                    "h"       : f"{cosmo.params['h']:0.6f}",
+                    "omegam"  : f"{cosmo.params['omegam']:0.6f}",
+                    "omegal"  : f"{cosmo.params['omegal']:0.6f}",
+                    "sigma8"  : f"{cosmo.params['sigma8']:0.6f}",
+                    "ns"      : f"{cosmo.params['ns']:0.6f}",
+                    "dRedTo"  : f"{self.params['dRedTo']}",
+                    "nSteps"  : f"{self.params['nSteps']}"
+        })
+        print(f"created {parfile}")
+
+        # slurm batch script
+        slurmfile = f"{jobdir}/launch.sh"
+        copytemplate(slurmtemp,slurmfile,{
+                    "tlimit"    : tlimit,
+                    "email"     : email,
+                    "nodes"     : nodes,
+                    "jobname"   : jobname,
+                    "runscript" : runscript,
+                    "lbox"      : lbox,
+                    "parfile"   : parfile
+            })
+        print(f"created {slurmfile}")
+
+        if sbatch:
+            subprocess.call(f"sbatch {slurmfile}", shell=True)
